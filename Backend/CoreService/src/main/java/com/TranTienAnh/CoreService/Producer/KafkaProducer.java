4d3154111735762543984f19e13f889dcc52a798
Trần Tiến Anh <anhtin44@gmail.com>
2026-06-17 21:21:12 +0700

    Docker: Setup Kafka & CoreService Producer

---
+++ Backend/CoreService/src/main/java/com/TranTienAnh/CoreService/Producer/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.TranTienAnh.CoreService.Producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
+
+@Service
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    public void sendMessage(String key, Object event) {
+        String message = objectMapper.writeValueAsString(event);
+        kafkaTemplate.send(topicName, key, message);
+    }
+}
